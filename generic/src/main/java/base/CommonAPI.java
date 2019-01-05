1ceaeadd335f7e5950263ca7d007858ec699a225
marufn17 <marufn17@gmail.com>
2019-01-05 14:51:14 -0500

    getDriver method updated for windows driver path

--- generic/src/main/java/base/CommonAPI.java
+++ generic/src/main/java/base/CommonAPI.java
@@ -77,10 +77,10 @@ public class CommonAPI {
                 driver = new FirefoxDriver();}
         } else if (os.contains("windows")) {
             if (browser.equalsIgnoreCase("chrome")) {
-                System.setProperty("webdriver.chrome.driver", "Driver/chromedriver.exe");
+                System.setProperty("webdriver.chrome.driver", "..\\Generic\\Drivers\\Windows\\chromedriver.exe");
                 driver = new ChromeDriver();
             } else if (browser.equalsIgnoreCase("Firefox")) {
-                System.setProperty("webdriver.gecko.driver", "Driver/geckodriver.exe");
+                System.setProperty("webdriver.gecko.driver", "..\\Generic\\Drivers\\Windows\\geckodriver.exe");
                 driver = new FirefoxDriver();}
         }
     }
