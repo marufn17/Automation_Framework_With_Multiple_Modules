bfe41efe59510a38cfed98e79f37f41bd05ec1a7
marufn17 <marufn17@gmail.com>
2019-01-05 00:31:33 -0500

    FooterPage Added

--- nbc/src/test/java/page/objects/HomePage.java
+++ nbc/src/test/java/page/objects/HomePage.java
@@ -3,8 +3,6 @@ import application.page.base.ApplicationPageBase;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.testng.annotations.Test;
-
 
 public class HomePage extends ApplicationPageBase {
 
