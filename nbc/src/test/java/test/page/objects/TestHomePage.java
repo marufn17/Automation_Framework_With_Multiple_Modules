07289633e6b8fa2ad5cb97f43c51a5c22c8b896b
marufn17 <marufn17@gmail.com>
2019-01-07 13:27:17 -0500

    test cases added to homepage and testhomepage

--- nbc/src/test/java/test/page/objects/TestHomePage.java
+++ nbc/src/test/java/test/page/objects/TestHomePage.java
@@ -1,6 +1,7 @@
 package test.page.objects;
 
 import application.page.base.ApplicationPageBase;
+import org.apache.commons.lang3.builder.ToStringExclude;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +17,6 @@ public class TestHomePage extends ApplicationPageBase {
 
     @BeforeMethod
     public void setUp(){homePage = PageFactory.initElements(driver,HomePage.class);}
-
     @Test
     public void homePageTitleTest(){
         String title = homePage.getHomePageTitle();
@@ -32,9 +32,8 @@ public class TestHomePage extends ApplicationPageBase {
         boolean display = homePage.SlideshowContainerDisplay();
         Assert.assertEquals(display,true);
     }
-
     @Test
-    public void barNameCheckHomePage(){
+    public void tabNameCheckHomePage(){
         List<String> expected = new ArrayList<>();
         expected.add("POPULAR SHOWS");
         expected.add("NEW EPISODES");
@@ -52,13 +51,44 @@ public class TestHomePage extends ApplicationPageBase {
         List<String> actual = homePage.spellCheckHomePage();
         Assert.assertEquals(expected,actual);
     }
-
-
-
-//    @Test
-//    public void clickSearch(){
-//        String text = homePage.clickSearch();
-//        Assert.assertEquals(text,"");
-//    }
-
+    @Test
+    public void clickSearch(){
+       String actual = homePage.checkSearchTab();
+       Assert.assertEquals(actual,"FEATURED SHOWS");
+    }
+    @Test
+    public void clickEpisodes(){
+        String actual = homePage.checkEpisodesTab();
+        Assert.assertEquals(actual,"FULL EPISODES");
+    }
+    @Test
+    public void clickSchedule(){
+        String actual = homePage.checkScheduleTab();
+        Assert.assertEquals(actual,"SCHEDULE");
+    }
+    @Test
+    public void clickShop() throws InterruptedException {
+        String actual = homePage.checkShopTab();
+        Assert.assertEquals(actual,"https://www.nbcstore.com/?cid=nbc_global_nav_shop");
+    }
+    @Test
+    public void clickApp(){
+        String actual = homePage.checkAppTab();
+        Assert.assertEquals(actual,"THE NBC APP");
+    }
+    @Test
+    public void clickLive(){
+        String actual = homePage.checkLive();
+        Assert.assertEquals(actual,"LINK PROVIDER");
+    }
+    @Test
+    public void clickNewsAndSports(){
+        boolean actual = homePage.newsFrame();
+        Assert.assertEquals(actual,true);
+    }
+    @Test
+    public void clickShows(){
+        boolean actual = homePage.showsFrame();
+        Assert.assertEquals(actual,true);
+    }
 }
