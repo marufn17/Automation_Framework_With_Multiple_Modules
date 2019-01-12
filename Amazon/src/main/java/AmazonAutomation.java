6d842bd1d01d9a3a6549ff40a47f1738ff62a314
ZibonIbrahim <zibonibrahim@yahoo.com>
2019-01-12 04:48:49 -0500

    updatedAmazon

--- Amazon/src/main/java/AmazonAutomation.java
+++ Amazon/src/main/java/AmazonAutomation.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -7,19 +9,33 @@ import java.util.concurrent.TimeUnit;
 
 public class AmazonAutomation {
     public static WebDriver driver = null;
+    String baseUrl;
 
     @BeforeMethod
-    public void seUP(){
+    public void seUP() throws InterruptedException {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://www.amazon.com");
+        baseUrl = "https://www.amazon.com/";
 
+        //Dynamic wait
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        driver.get("https://www.amazon.com");//enter url
+        Thread.sleep(500);
+
+        driver.manage().window().maximize();//maximise window
+        driver.manage().deleteAllCookies();//delete all cookies!
     }
+
 
     @AfterMethod
-    public void cleanUp(){
+    public void cleanUp()
+    {
         driver.close();
     }
+
+
 }
 
 
