cf038b394a78ae17f052d890386e37d95e4ed841
ZibonIbrahim <zibonibrahim@yahoo.com>
2019-01-11 22:30:28 -0500

    AmazonAutomation subs

---
+++ Amazon/src/main/java/AmazonAutomation.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class AmazonAutomation {
+    public static WebDriver driver = null;
+
+    @BeforeMethod
+    public void seUP(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.amazon.com");
+
+    }
+
+    @AfterMethod
+    public void cleanUp(){
+        driver.close();
+    }
+}
+
+
+
