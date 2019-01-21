755710d71bd4d9d972f214bafc7618d7ab63bad0
kazi2018 <rahmanhkazi@gmail.com>
2019-01-20 21:24:43 -0500

    Register page code modified

--- Ebay/src/main/java/Pages/RegisterPage.java
+++ Ebay/src/main/java/Pages/RegisterPage.java
@@ -4,6 +4,7 @@ import base.CommonAPI;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import report.TestLogger;
 
 public class RegisterPage extends CommonAPI {
     public RegisterPage() {
@@ -11,22 +12,24 @@ public class RegisterPage extends CommonAPI {
     }
     @FindBy(css = "#firstname")
     WebElement firstName;
-    public void setFirstName(String FirstName) {
-        firstName.sendKeys(FirstName);
-    }
     @FindBy(css = "#lastname")
     WebElement lastName;
-    public void setLastName(String LastName) {
-        lastName.sendKeys(LastName);
-    }
     @FindBy(css = "#email")
     WebElement email;
-    public void setEmail(String Email) {
-        email.sendKeys(Email);
-    }
     @FindBy(css = "#PASSWORD")
     WebElement password;
-    public void setPassword() {
+    @FindBy(css = "#ppaFormSbtBtn")
+    WebElement createAccountButton;
+    @FindBy(css = "#ocs-inflow > h3")
+    WebElement userdifferntemail;
+    public String setRegisterPage() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        firstName.sendKeys("Safwan");
+        lastName.sendKeys("Rahman");
+        email.sendKeys("email@email.com");
         password.sendKeys("abc@123");
+        createAccountButton.click();
+        String string = createAccountButton.getText();
+        return string;
     }
 }
--- Ebay/src/test/java/Test/TestRegisterPage.java
+++ Ebay/src/test/java/Test/TestRegisterPage.java
@@ -4,6 +4,7 @@ import Pages.HomePage;
 import Pages.RegisterPage;
 import base.CommonAPI;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -17,24 +18,10 @@ public class TestRegisterPage extends CommonAPI {
         homePage = PageFactory.initElements(driver, HomePage.class);
         homePage.createaccttab();
     }
-
-    @Test(priority = 1)
-    public void firstNamefield() {
-        registerPage.setFirstName("Safwan");
-    }
-
-    @Test(priority = 2)
-    public void lastNamefield() {
-        registerPage.setLastName("Rahman");
-    }
-
-    @Test(priority = 3)
-    public void emailfield() {
-        registerPage.setEmail("safwan@pnt.com");
-    }
-
     @Test(priority = 4)
-    public void passwordfield() {
-        registerPage.setPassword();
+    public void registerPage() {
+     String actual = registerPage.setRegisterPage();
+     String expected = "Create account";
+        Assert.assertEquals(actual,expected);
     }
 }
